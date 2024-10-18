b36354bd8ca15d3aa4b37d1ac238523a685eff6c
jacob Payne <jacob.pa7ne@Gmail.com>
2024-10-18 17:42:26 -0500

    added email slot to driver model

--- RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/models/Driver.java
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/models/Driver.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
@@ -22,6 +23,10 @@ public class Driver {
 	@NotEmpty(message = "Last name is required!")
 	@Size(min = 3,max = 15,message = "Last name must be between 3 and 15 characters")
 	private String lastName;
+	
+	@NotEmpty(message = "Email is required!")
+	@Email(message = "Please enter a valid email!")
+	 private String email;
 	
 	@NotEmpty(message = "Password is required!")
 	@Size(min = 8,max = 128,message = "Password must be between 8 and 200 characters" )
