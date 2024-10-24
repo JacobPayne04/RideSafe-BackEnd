f7f810f91942198cff3201ebdb2305fe3dbc0029
Megan Zehrt <zehrtmegan@gmail.com>
2024-10-24 17:32:35 -0500

    Fixed Passenger Repo

--- RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/models/Passenger.java
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/models/Passenger.java
@@ -28,6 +28,46 @@ public class Passenger {
     private String confirm;
 
     public Passenger() {}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
     
     
 }
--- RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/repositorys/PassengerRepository.java
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/repositorys/PassengerRepository.java
@@ -1,5 +1,13 @@
 package com.Jacob.ridesafebackend.repositorys;
 
-public class PassengerRepository {
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Jacob.ridesafebackend.models.Passenger;
+
+@Repository
+public interface PassengerRepository extends CrudRepository<Passenger, String> {
+	List<Passenger> findAll();
 }
