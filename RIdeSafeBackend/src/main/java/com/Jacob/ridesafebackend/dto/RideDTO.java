44c1b7a30d2589d5d3e38afb93fda2d4cff6220a
jacob Payne <jacob.pa7ne@Gmail.com>
2025-03-17 15:39:01 -0500

    Make DTO object for payment intent

--- RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/dto/RideDTO.java
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/dto/RideDTO.java
@@ -2,4 +2,23 @@ package com.Jacob.ridesafebackend.dto;
 
 public class RideDTO {
 
+	private int passengerCount;
+	private int rate;
+	public int getPassengerCount() {
+		return passengerCount;
+	}
+	public void setPassengerCount(int passengerCount) {
+		this.passengerCount = passengerCount;
+	}
+	public int getRate() {
+		return rate;
+	}
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	
+	
+	
+	
+	
 }
