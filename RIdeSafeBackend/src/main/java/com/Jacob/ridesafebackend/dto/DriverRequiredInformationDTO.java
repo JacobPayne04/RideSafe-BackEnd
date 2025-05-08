375d3b2cc0a3662855e6f8470c57ed814aae88c2
jacob Payne <jacob.pa7ne@Gmail.com>
2025-05-07 19:28:28 -0500

    Driver dto for legal information sign up

---
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/dto/DriverRequiredInformationDTO.java
@@ -0,0 +1,41 @@
+package com.Jacob.ridesafebackend.dto;
+
+public class DriverRequiredInformationDTO {
+	 private String firstName;
+	    private String lastName;
+	    private String licensePlate;
+	    private boolean acceptedTerms;
+	    private String eSign;
+		public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getLastName() {
+			return lastName;
+		}
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+		public String getLicensePlate() {
+			return licensePlate;
+		}
+		public void setLicensePlate(String licensePlate) {
+			this.licensePlate = licensePlate;
+		}
+		public boolean isAcceptedTerms() {
+			return acceptedTerms;
+		}
+		public void setAcceptedTerms(boolean acceptedTerms) {
+			this.acceptedTerms = acceptedTerms;
+		}
+		public String geteSign() {
+			return eSign;
+		}
+		public void seteSign(String eSign) {
+			this.eSign = eSign;
+		}
+	    
+	    
+}
