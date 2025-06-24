e37216fb568e0d140bfd98e1602a06db8f810592
jacob Payne <jacob.pa7ne@Gmail.com>
2025-06-24 18:13:47 -0500

    added filter config ready for testing

---
+++ RIdeSafeBackend/src/main/java/com/Jacob/ridesafebackend/config/FitlerConfig.java
@@ -0,0 +1,34 @@
+package com.Jacob.ridesafebackend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.Jacob.ridesafebackend.filters.jwtAuthenticationFilter;
+
+@Configuration
+public class FitlerConfig {
+
+	@Bean
+	 public FilterRegistrationBean<jwtAuthenticationFilter> registerJwtFilter(jwtAuthenticationFilter filter) {
+	        FilterRegistrationBean<jwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+
+	        registrationBean.setFilter(filter);
+
+	        // 🔐 Define which routes this filter protects
+	        registrationBean.addUrlPatterns( 
+	            "/edit/driver/*",
+	            "/create-Payment-Intent",
+	            "/refund",
+	            "/rides/save",
+	            "/update-ride-payment"
+	        );
+
+	        registrationBean.setOrder(1); // Optional: set filter order if you have others
+
+	        return registrationBean;
+	    }
+	
+	
+	
+}
